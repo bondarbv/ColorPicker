cfa6dd74264678dc1ff2e8391531aea020473a35
bondarbv <92814470+bondarbv@users.noreply.github.com>
2022-05-20 23:42:56 +0300

    Created a project

---
+++ ColorPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Bohdan on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
